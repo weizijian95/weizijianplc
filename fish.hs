db3612bfcb8f8344128a538491cedb92adcf452e
Fish <1051351544@qq.com>
2020-12-09 16:46:35 +0800

    可执行版本v1.0

--- fish.hs
+++ fish.hs
@@ -134,19 +134,5 @@ specGuide specfile=
                                              specGuide specfile}
                                 }
                    }
-}
-}
-{-     inh <- openFile "input.txt" ReadMode
-    outh <- openFile "output.txt" WriteMode
-    mainloop inh outh
-    hClose inh
-    hClose outh
-
-mainloop :: Handle -> Handle -> IO ()
-mainloop inh outh =
-    do ineof <- hIsEOF inh
-       if ineof
-       then return ()
-       else do inpStr <- hGetLine inh
-               hPutStrLn outh (map toUpper inpStr)
-               mainloop inh outh -}
+                }
+        }
