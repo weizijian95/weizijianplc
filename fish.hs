e48f22546186e894acdf503a34f41d11a4338122
Fish <1051351544@qq.com>
2020-12-10 17:09:41 +0800

    修改了规约引导，适配全部模式和事件组合

--- fish.hs
+++ fish.hs
@@ -1,7 +1,4 @@
 import System.IO
-import Data.Char(toUpper)
-魏梓健在家修改了一次a
-在家修改
 main :: IO ()
 main = do specfile <- openFile "spec.txt" ReadWriteMode
           specGuide specfile
@@ -9,132 +6,84 @@ main = do specfile <- openFile "spec.txt" ReadWriteMode
 specGuide:: Handle->IO()
 specGuide specfile= 
         do {
-                putStrLn "请根据向导完成规约描述，若所有规约已输入完成，请输入done";
-                putStrLn "(a)要求控制对象在整个区间内持续成立 (b)要求控制对象在整个区间内持续不成立 (c)要求控制对象在区间左端点成立，其余时刻均不成立 （done）规约输入完成";
-                ans<-getLine;
-                let info_prop= case ans of 
-                                    "a"->"Universality"
-                                    "b"->"Absence"
-                                    "c"->"Existence"
-                                    _->ans                                   
-                in do {case ans of 
-                   "done"->do {putStrLn "规约文件spec.txt生成完成" ;  return()} 
-                   _ ->do {
-                           putStrLn "1.选择对控制对象成立范围的要求（a）无条件 （b）在左参照点成立后 （c）在两个参照点之间 (d)与参照点同时成立";
-                           ans1<-getLine;
-                           case ans1 of
-                                "a"-> do {
-                                        putStrLn "请输入控制对象名称";
-                                        info_p<-getLine;
-                                        putStrLn "1_1.选择控制对象成立范围的起始点和终止点情况（a）起始点为系统初始时刻，无终止点 （b）起始点为 t1 时刻，无终止点 （c） 起始点为 t1 时刻，终止点为 t2 时刻";
-                                        ans1_1<-getLine;
-                                        case ans1_1 of 
-                                             "a"->do {hPutStrLn specfile ("Global delayL=0，delayR=null "++info_prop ++" "++info_p);
-                                              specGuide specfile}                               
-                                             "b"->do {putStrLn "请输入t1值";
-                                              info_t1<-getLine;
-                                              hPutStrLn specfile ("Global delayL="++info_t1++" delayR=null "++info_prop++" "++info_p);
-                                              specGuide specfile}
-                                             "c"->do {putStrLn "请输入t1值";                                            
-                                              info_t1<-getLine;
-                                              putStrLn "请输入t2值";
-                                              info_t2<-getLine;
-                                              hPutStrLn specfile ("Global delayL="++info_t1++" delayR="++info_t2++" "++info_prop++" "++info_p);
-                                              specGuide specfile   }
-                                }
-                                "b"-> do {
-                                        putStrLn "请输入控制对象名称";
-                                        info_p<-getLine;
-                                        putStrLn "请输入左参照点名称";
-                                        info_q<-getLine;
-                                        putStrLn $ "1_2.选择控制对象成立范围的起始点和终止点情况（a）起始点为"++info_q++"，无终止点 （b）起始点为"++info_q++"后 t1 时刻，无终止点 （c） 起始点为"++info_q++"后 t1 时刻，终止点为"++info_q++"后 t2 时刻";
-                                        ans1_2<-getLine;
-                                        case ans1_2 of 
-                                             "a"->do {hPutStrLn specfile ("After "++info_q++" delayL=0，delayR=null "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "b"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" delayL="++info_t1++" delayR=null "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "c"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             putStrLn "请输入t2值";
-                                             info_t2<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" delayL="++info_t1++" delayR="++info_t2++" "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                }
-                                "c"->do {
-                                        putStrLn "请输入控制对象名称";
-                                        info_p<-getLine;
-                                        putStrLn "请输入左参照点名称";
-                                        info_q<-getLine;
-                                        putStrLn "请输入右参照点名称";
-                                        info_r<-getLine;
-                                        putStrLn $ "1_3.选择控制对象成立范围的起始点和终止点情况（a）起始点为"++info_q++"，终止点为"++info_r++" （b）起始点为"++info_q++"后 t1 时刻，终止点为"++info_r++" （c）起始点为"++info_q++"，终止点为"++info_q++"后t2时刻或"++info_r++" （d）起始点为"++info_q++"，终止点为"++info_r++"后t3时刻（e） 起始点为"++info_q++"后 t1 时刻，终止点为"++info_q++"后 t2 时刻或"++info_r++" (f)起始点为"++info_q++"后 t1 时刻，终止点为"++info_r++"后t3时刻 ";
-                                        ans1_3<-getLine;
-                                        case ans1_3 of 
-                                             "a"->do {hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL=0，delayR=null delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "b"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL="++info_t1++"，delayR=null delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "c"->do {putStrLn "请输入t2值";
-                                             info_t2<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL=0，delayR="++info_t2++" delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile   }                 
-                                             "d"->do {putStrLn "请输入t3值";
-                                             info_t3<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL=0，delayR=null delayRE="++info_t3++" "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "e"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             putStrLn "请输入t2值";
-                                             info_t2<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL="++info_t1++"，delayR="++info_t2++" delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "f"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             putStrLn "请输入t3值";
-                                             info_t3<-getLine;
-                                             hPutStrLn specfile ("After "++info_q++" Until "++info_r++"  delayL="++info_t1++"，delayR=null delayRE="++info_t3++" "++info_prop++" "++info_p);
-                                             specGuide specfile }
-                                }
-                                "d"->do {
-                                        putStrLn "请输入控制对象名称";
-                                        info_p<-getLine;
-                                        putStrLn "请输入参照点名称";
-                                        info_q<-getLine;
-                                        putStrLn $ "1_4.选择控制对象成立范围的起始点和终止点情况（a）起始点为"++info_q++"，终止点为q不再成立的时刻 （b）起始点为"++info_q++"后 t1 时刻，终止点为q不再成立的时刻 （c）起始点为"++info_q++"，终止点为"++info_q++"后t2时刻或q不再成立的时刻 （d）起始点为"++info_q++"，终止点为q不再成立的时刻后t3时刻（e） 起始点为"++info_q++"后 t1 时刻，终止点为"++info_q++"后 t2 时刻或q不再成立的时刻 (f)起始点为"++info_q++"后 t1 时刻，终止点为q不再成立的时刻后t3时刻 ";
-                                        ans1_4<-getLine;
-                                        case ans1_4 of 
-                                             "a"->do {hPutStrLn specfile ("When "++info_q++" delayL=0，delayR=null delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "b"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             hPutStrLn specfile ("When "++info_q++"  delayL="++info_t1++"，delayR=null delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "c"->do {putStrLn "请输入t2值";
-                                             info_t2<-getLine;
-                                             hPutStrLn specfile ("When "++info_q++"  delayL=0，delayR="++info_t2++" delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "d"->do {putStrLn "请输入t3值";
-                                             info_t3<-getLine;
-                                             hPutStrLn specfile ("When "++info_q++"  delayL=0，delayR=null delayRE="++info_t3++" "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "e"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             putStrLn "请输入t2值";
-                                             info_t2<-getLine;
-                                             hPutStrLn specfile ("When "++info_q++"  delayL="++info_t1++"，delayR="++info_t2++" delayRE=0 "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                             "f"->do {putStrLn "请输入t1值";
-                                             info_t1<-getLine;
-                                             putStrLn "请输入t3值";
-                                             info_t3<-getLine;
-                                             hPutStrLn specfile ("When "++info_q++"  delayL="++info_t1++"，delayR=null delayRE="++info_t3++" "++info_prop++" "++info_p);
-                                             specGuide specfile}
-                                }
-                   }
-                }
+                putStrLn "请根据向导完成规约描述";
+                putStrLn "请输入新规约的控制对象名称，若所有规约已输入完成，请输入done";
+                info_p<-getLine;
+                case info_p of 
+                     "done"-> do {putStrLn "规约文件spec.txt生成完毕"; return ()}   
+                     _->do 
+                             {putStrLn "请选择规约控制区间的左端点类型 (a)程序启动时 (b)程序启动后t1时间 (c)某事件发生时 (d)某事件发生后t1时间 ";
+                              ans1<-getLine ;
+                              putStrLn "请选择规约控制区间的右端点类型 (a)无右端点 (b)左端点发生后t2时间 (c)某事件发生时 (d)某事件发生后t3时间 (e)左端点发生后t2时间或某事件发生时 (f)左端点发生后t2时间或某事件发生后t3时间";
+                              ans2<-getLine ;
+                              putStrLn "请选择对规约控制对象的要求 (a)在控制区间内一直发生 (b)在控制区间内一直不发生 (c)仅在控制区间的左端点发生";
+                              ans3<-getLine ;
+                              let {answer=ans1++ans2;info_prop=case ans3 of
+                                                                    "a"->"Universality"
+                                                                    "b"->"Absence"
+                                                                    "c"->"Existence" } in
+                                 do {case answer of  
+                                        "aa"->do {hPutStrLn specfile ("Global,"++info_prop++" "++info_p)}
+                                        "ab"->do {putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("Global delayR="++info_t2++","++info_prop++" "++info_p)}
+                                        "ac"->do {putStrLn "请输入右端点事件"; info_r<-eventGuide;
+                                          hPutStrLn specfile ("Before "++info_r++","++info_prop++" "++info_p)}
+                                        "ad"->do {putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++" delay RE="++info_t3++","++info_prop++" "++info_p)}  
+                                        "ae"->do {putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++" delay R="++info_t2++","++info_prop++" "++info_p)}  
+                                        "af"->do {putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;putStrLn"请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++" delay R="++info_t2++" delay RE="++info_t3++","++info_prop++" "++info_p)}
+                                        
+                                        "ba"->do {putStrLn "请输入t1"; info_t1<-getLine;hPutStrLn specfile ("Global,"++"delayL="++info_t1++info_prop++" "++info_p)}
+                                        "bb"->do {putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("Global "++"delayL="++info_t1++"delayR="++info_t2++","++info_prop++" "++info_p)}
+                                        "bc"->do {putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide;
+                                          hPutStrLn specfile ("Before "++info_r++" "++"delayL="++info_t1++","++info_prop++" "++info_p)}
+                                        "bd"->do {putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++"delayL="++info_t1++" delay RE="++info_t3++","++info_prop++" "++info_p)}  
+                                        "be"->do {putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++" delayL="++info_t1++" delay R="++info_t2++","++info_prop++" "++info_p)}  
+                                        "bf"->do {putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;putStrLn"请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("Before "++info_r++" delayL="++info_t1++" delay R="++info_t2++" delay RE="++info_t3++","++info_prop++" "++info_p)}   
+                                        
+                                        "ca"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;hPutStrLn specfile ("After "++info_l++","++info_prop++" "++info_p)}
+                                        "cb"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" delayR="++info_t2++","++info_prop++" "++info_p)}
+                                        "cc"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入右端点事件"; info_r<-eventGuide;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++","++info_prop++" "++info_p)}
+                                        "cd"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delay RE="++info_t3++","++info_prop++" "++info_p)}  
+                                        "ce"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delay R="++info_t2++","++info_prop++" "++info_p)}  
+                                        "cf"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;putStrLn"请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delay R="++info_t2++" delay RE="++info_t3++","++info_prop++" "++info_p)}
+                                        
+                                        "da"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;hPutStrLn specfile ("After "++info_l++" delayL="++info_t1++","++info_prop++" "++info_p)}
+                                        "db"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" delayL="++info_t1++" delayR="++info_t2++","++info_prop++" "++info_p)}
+                                        "dc"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delayL="++info_t1++","++info_prop++" "++info_p)}
+                                        "dd"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delayL="++info_t1++" delay RE="++info_t3++","++info_prop++" "++info_p)}  
+                                        "de"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delayL="++info_t1++" delay R="++info_t2++","++info_prop++" "++info_p)}  
+                                        "df"->do {putStrLn "请输入左端点事件"; info_l<-eventGuide;putStrLn "请输入t1"; info_t1<-getLine;putStrLn "请输入右端点事件"; info_r<-eventGuide; putStrLn "请输入t2"; info_t2<-getLine;putStrLn"请输入t3"; info_t3<-getLine;
+                                          hPutStrLn specfile ("After "++info_l++" Until "++info_r++" delayL="++info_t1++" delay R="++info_t2++" delay RE="++info_t3++","++info_prop++" "++info_p)} 
+
+                                        _->do {putStrLn "输入非法，请检查后重新录入"}                                     
+                                ;specGuide specfile } 
+
+                             }
         }
+eventGuide::IO String
+eventGuide=do {
+        putStrLn "请选择事件类型 (a)发生某原子事件,即p (b)不发生某事件，即not e (c)某两事件同时发生，即e1 and e2 (d)某两事件发生其一，即e1 or e2 ";
+        ans<-getLine;
+        case ans of  
+                "a"->do {putStrLn"请录入发生的事件名称"; p<-getLine; return p}
+                "b"->do {putStrLn"请录入不发生的事件"; p<-eventGuide;return ("not ("++p++")")}
+                "c"->do {putStrLn"请录入事件1"; p1<-eventGuide; putStrLn $"当前事件为："++p1++" and 事件2"; putStrLn"请录入事件2"; p2<-eventGuide;return ("("++p1++" and "++p2++")")}
+                "d"->do {putStrLn"请录入事件1"; p1<-eventGuide; putStrLn $"当前事件为："++p1++" or 事件2"; putStrLn"请录入事件2"; p2<-eventGuide;return ("("++p1++" or "++p2++")")}
+                _-> do {putStrLn"非法输入，请重新输入"; p<-eventGuide;return p}
+}
